91f3bfdf9835bd9ed1cd6ae82c1e2878cbeac5cf
liwenxin-jam <mxlwx@126.com>
2019-07-18 10:40:38 +0800

    更新文档

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
   <div id="app">
     <router-view />
+    <!-- <keep-alive>
+      <router-view v-if="$route.meta.keepAlive"></router-view>
+    </keep-alive>
+    <router-view v-if="!$route.meta.keepAlive"></router-view> -->
+    <!-- <transition name="fade-transform" mode="out-in">
+      <keep-alive :include="['A', 'B']">
+        <router-view :key="key" />
+      </keep-alive>
+    </transition> -->
   </div>
 </template>
