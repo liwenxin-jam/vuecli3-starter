c07823a1f134b62c6dc0f056d3715366da8646e2
liwenxin-jam <mxlwx@126.com>
2019-07-19 17:35:49 +0800

    add communication

---
+++ src/views/communication/components/child/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="user in users">{{user}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'child',
+    props: {
+      users: {
+        type: Array,
+        default: () => [],
+        required: true
+      }
+    }
+  }
+</script>
--- src/views/communication/index.vue
+++ src/views/communication/index.vue
@@ -1,4 +1,19 @@
 <!-- 父组件 -->
 <template>
-  <div></div>
+  <div>
+    <child v-bind:users="users"></child>
+  </div>
 </template>
+
+<script>
+  import child from "./components/child"
+  export default {
+    name: 'communication',
+    components: { child },
+    data() {
+      return {
+        users: ["Henry", "Bucky", "Emily"]
+      }
+    }
+  }
+</script>
